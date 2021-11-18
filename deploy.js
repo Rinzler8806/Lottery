a05aff60ecd8725e29fd9ed21b2d62d314c72ca9
Rinzler8806 <shane.rogers916@gmail.com>
2021-11-17 20:16:36 -0800

    initial commit after creating inbox.sol compile.js deploy.js and json files

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+  'thrive garment speed guilt release quality confirm discover club eight install rhythm',
+  'https://rinkeby.infura.io/v3/6aa151470ec44067830852139f283c7d'
+);
+ 
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
